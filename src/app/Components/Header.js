c8daaf4b67d0cbdccd32ebc2fb73b0a5247681eb
WebH <107492632+mwebh@users.noreply.github.com>
2023-07-26 11:00:20 +0100

    -

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -2,33 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import Logo from "../../Images/Listabl.png"
 
-export default function Header({color}) {
-
-    const links = [
-        // {
-        //     name: "Home",
-        //     link: "/"
-        // }
-    ]
+export default function Header({color, list}) {
 
   return (
     <div
-        className='flex justify-between w-screen h-max px-10 py-4 items-center relative top-0 transition-colors duration-500'
+        className='flex justify-between max-w-screen h-max px-10 py-4 items-center relative top-0 transition-colors duration-500'
         style={{backgroundColor: color}}
     >
         <div>
             <img src={Logo.src} alt="Listabl" className=' max-h-10' />
         </div>
-        <div className='flex gap-4'>
-            {
-                links.map(link => {
-                    return (
-                        <Link href="/" key={link.name} className='text-white shadow-lg px-4 py-2 hover:bg-neutral-600 rounded transition-all duration-200 active:bg-neutral-800'>
-                            {link.name}
-                        </Link>
-                    )
-                })
-            }
+        <div className=' px-4 py-2 bg-neutral-50 rounded'>
+            {list && <p>Items: {list.length}</p>}
+            {!list && <p>Items: 0</p>}
         </div>
 
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,20 @@ import IncrementButton from "./Components/IncrementButton";
 import Header from "./Components/Header";
 import Link from "next/link";
 import Modal from "./Components/Modal";
+import ItemNotification from "./Components/ItemNotification";
 
 export default function Home() {
   
   const [list, setList] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [color, setColor] = useState("#66BDFF");
+  const [checkbox, setCheckbox] = useState(false)
   const [modal, setModal] = useState(false)
+  const [deleted, setDeleted] = useState(false)
   
   useEffect(() => {
     setList(JSON.parse(localStorage.getItem("Lists")))
+    console.log(list)
     setColor(localStorage.getItem("LastColour"))
   },[])
 
@@ -23,7 +27,7 @@ export default function Home() {
       name: inputValue,
       id: list ? list.at(list.length - 1).id + 1 : 1,
       color: color,
-      checked: false
+      completed: false
     }
     if (list) {
       const newList = [
@@ -58,35 +62,45 @@ export default function Home() {
       setList(null)
       localStorage.removeItem("Lists")
     }
+
+    setDeleted(true)
+    setTimeout(() => {
+      setDeleted(false)
+    },3000)
   }
 
 
   return (
     <main className="flex flex-col gap-10 bg-neutral-50">
-      <Header color={color} />
-      <div className="flex flex-col px-6 md:px-10 sticky top-0 z-30 py-6 bg-neutral-50">
+      <Header color={color} list={list} />
+      <div className="flex flex-col px-6 md:px-10 sticky top-0 z-30 py-6 bg-neutral-50 max-w-full">
         <IncrementButton inputValue={inputValue} setInputValue={setInputValue} addList={addList} color={color} setColor={setColor} />
       </div>
       {
         list && (
-          <div className="flex flex-wrap gap-10 px-6 md:px-10 mb-32 max-w-full">
+          <div className="flex flex-wrap gap-6 px-6 md:px-10 mb-32 max-w-full justify-start">
             {
               list.map(listItem => {
                 return (
                   <div 
                     key={listItem.id}
-                    className={`fade-in flex justify-between items-center px-6 py-6 shadow-md rounded-md w-1/4 max-w-md min-w-[300px] mx-auto h-max gap-4`}
+                    className={`fade-in flex justify-between items-center px-6 py-6 shadow-md rounded-md w-1/4 max-w-md min-w-[300px] h-max gap-4`}
                     style={{backgroundColor: listItem.color}}
                   >
-                    <p className=" px-4 py-2 rounded bg-white/80 shadow-md relative text-base">{listItem.name}</p>
+                    <div className="relative">
+                      <p 
+                        className={`px-4 py-2 rounded bg-neutral-50 shadow-md relative text-base`}>
+                        {listItem.name}
+                      </p>
+
+                    </div>
                     <button
-                      className=" hover:text-red-500 pointer-events-auto bg-white/80 h-10 aspect-square flex justify-center items-center rounded-full shadow-md"
+                      className=" hover:text-red-500 pointer-events-auto bg-neutral-50 hover:bg-neutral-100 hover:shadow-lg hover:scale-105 transition-all duration-200 h-10 aspect-square flex justify-center items-center rounded-full shadow-md"
                       onClick={() => deleteList(listItem.id)}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-green-500">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                       </svg>
-
                     </button>
                   </div>
                 )
@@ -101,13 +115,13 @@ export default function Home() {
             <h3 
               className=" px-6 py-4 rounded-xl bg-neutral-800 text-neutral-50 text-base"
               >
-              You&apos;ve not got any items yet, simply type your items in the field above and choose a colour!
+              You&apos;ve not got any items yet, simply type in your items above and choose a colour!
             </h3>
           </div>
         )
       }
       <button 
-        className=" fixed bottom-6 left-4 p-2 rounded-full"
+        className=" fixed bottom-6 left-6 p-2 rounded-full"
         style={{backgroundColor: color}}
         onClick={() => setModal(!modal)}
       >
@@ -116,6 +130,7 @@ export default function Home() {
         </svg>
       </button>
       <Modal modal={modal} setModal={setModal} color={color} />
+      <ItemNotification deleted={deleted} setDeleted={setDeleted} notification="Item Completed!" />
     </main>
   )
 }
